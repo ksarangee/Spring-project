3e111fc3fde20d90757e8e0c68bba3c4c3ab1725
Sarang Kim <ksarang0104@gmail.com>
2024-08-30 16:18:35 +0900

    12.2.1 done (edited GET with service)

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,19 +15,19 @@ import java.util.List;
 @Slf4j //로그 찍을 수 있게 추가
 @RestController //REST API용 컨트롤러
 public class ArticleApiController {
-    @Autowired
-    private ArticleRepository articleRepository;
+    @Autowired //생성 객체를 가져와 연결
+    private ArticleService articleService;
 
     //GET
-    @GetMapping("/api/articles")
+    @GetMapping("/api/articles") //서비스를 통해 데이터 가져오기
     public List<Article> index() {
-        return articleRepository.findAll();
+        return articleService.index();
     }
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable("id") Long id) { //id는 요청 url에서 가져 오므로 매개변수 앞에 @PathVariable 붙이기
-        return articleRepository.findById(id).orElse(null); //해당 엔티티 없으면 null 반환
+        return articleService.show(id); //서비스가 조회 요청 처
     }
-
+/*
     //POST
     @PostMapping("/api/articles")
     public Article create(@RequestBody ArticleForm dto) { //JSON 데이터를 받아 와햐 하니깐 @RequestBody 추가
@@ -66,5 +67,5 @@ public class ArticleApiController {
         //3. 대상 삭제
         articleRepository.delete(target);
         return ResponseEntity.status(HttpStatus.OK).build(); //build()는 body(null)과 동일
-    }
+    }*/
 }
